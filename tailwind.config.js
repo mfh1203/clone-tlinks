e4eb6a5dc91dbb5fbae12710faca7198eb59cb72
mfh1203 <mfh.1203@gmail.com>
2021-09-02 14:46:09 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'bookmark-blue':'#376eb3',
+        'bookmark-sky':'#e6eef3',
+      },
+      height:{
+        'xl':'20rem',
+      },
+    },
+    container:{
+      center:true,
+      padding:'2rem',
+
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
